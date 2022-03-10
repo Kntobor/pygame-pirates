7e0af358a3daf59fa05a0080fb2680a5a1e0178f
Rohan <rohan.s.malhi@gmail.com>
2022-03-09 22:10:46 -0800

    added (broken) animations & animation states

--- code/level.py
+++ code/level.py
@@ -63,10 +63,17 @@ class Level:
                 if player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
+                    player.onGround = True
                 elif player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
                     player.jumping = False
+                    player.onCeiling = True
+
+        if player.onGround and player.direction.y < 0 or player.direction.y > 1:
+            player.onGround = False
+        if player.onCeiling and player.direction.y > 0:
+            player.onCeiling = False
 
     def run(self):
         self.tiles.update(self.worldShift)
